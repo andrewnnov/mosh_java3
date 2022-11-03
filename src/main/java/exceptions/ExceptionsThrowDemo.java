4b49b5a79ea39bfe5a500ec76c7d2e058bdffee8
andrewnnov <andrewnnov@yandex.ru>
2022-11-03 11:50:39 +0300

    Custom exception

--- src/main/java/exceptions/ExceptionsThrowDemo.java
+++ src/main/java/exceptions/ExceptionsThrowDemo.java
@@ -7,11 +7,11 @@ public class ExceptionsThrowDemo {
     public static void show() throws IOException {
         var account = new Account();
         try {
-            account.deposit(-1);
-        } catch (IOException e) {
-            System.out.println("Logging");
-            throw e;
+            account.withdraw(10);
+        } catch (InsufficientFundsException e) {
+            System.out.println(e.getMessage());
         }
-
     }
+
+
 }
