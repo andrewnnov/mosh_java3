f8c17a90d5d7139b41875a6ab9f7fb6754698cf8
andrewnnov <andrewnnov@yandex.ru>
2022-11-03 12:04:52 +0300

    Chaining exception

--- src/main/java/exceptions/ExceptionsThrowDemo.java
+++ src/main/java/exceptions/ExceptionsThrowDemo.java
@@ -8,8 +8,9 @@ public class ExceptionsThrowDemo {
         var account = new Account();
         try {
             account.withdraw(10);
-        } catch (InsufficientFundsException e) {
-            System.out.println(e.getMessage());
+        } catch (AccountException e) {
+            var cause = e.getCause();
+            System.out.println(cause.getMessage());
         }
     }
 
