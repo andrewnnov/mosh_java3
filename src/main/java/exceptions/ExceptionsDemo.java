48238cf5039adf3ddaa958c9a454a2f3e593f36a
andrewnnov <andrewnnov@yandex.ru>
2022-11-03 11:11:04 +0300

    The try-with-resources statement

--- src/main/java/exceptions/ExceptionsDemo.java
+++ src/main/java/exceptions/ExceptionsDemo.java
@@ -3,32 +3,14 @@ package exceptions;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Locale;
-import java.util.logging.SimpleFormatter;
 
 public class ExceptionsDemo {
 
     public static void show() {
-
-        try {
-            var reader = new FileReader("hello.txt");
+        try(var reader = new FileReader("file.txt")) {
             var value = reader.read();
-            new SimpleDateFormat().parse("");
-            System.out.println("Files opened");
-        } catch (FileNotFoundException ex) {
-            System.out.println("File does not exist.");
-            System.out.println(ex.getMessage());
         } catch (IOException ex) {
             System.out.println("Could not read data.");
-        } catch (ParseException e) {
-            e.printStackTrace();
         }
-
-    }
-
-    public static void sayHello(String name) {
-        //System.out.println(name.toUpperCase());
     }
 }
